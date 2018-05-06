382ea5024881c184243d5dbbb4b832c2d6354c3e
Vishal Mudhiraj <vishalmudhiraj71@gmail.com>
2018-05-06 12:19:53 +0530

    initial commit

---
+++ src/Swapping.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vishal
+ */
+public class Swapping {
+    public static void main(String args[]){
+        int first = 7;
+        int second = 19;
+        //write your logic here
+        
+        System.out.println("first="+first+" second="+second);
+    }
+}
