610a08edb9dbc287c49d10366d4fd872abe14104
Vishal Mudhiraj <vishalmudhiraj71@gmail.com>
2018-05-06 13:23:32 +0530

    Fighting

---
+++ src/Fighting.java
@@ -0,0 +1,66 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vishal
+ */
+public class Fighting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Attack Points of Monster:");
+        int monsterAttack = sc.nextInt();
+        System.out.println("Defense Points of Monster:");
+        int monsterDefense = sc.nextInt();
+        System.out.println("Damage of Monster:");
+        int monsterDamage = sc.nextInt();
+        System.out.println("Life Points of Monster:");
+        int monsterLife = sc.nextInt();
+        
+        System.out.println("Your Attack Points:");
+        int yourAttack = sc.nextInt();
+        System.out.println("Your Defense Points:");
+        int yourDefense = sc.nextInt();
+        System.out.println("Your Damage:");
+        int yourDamage = sc.nextInt();
+        System.out.println("Your Life Points:");
+        int yourLife = sc.nextInt();
+        
+        Random generator = new Random();
+        boolean attacker = generator.nextBoolean();
+        if (attacker) {
+            System.out.println("You attack");
+            int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
+            int attackValue = yourAttack + dice;
+            System.out.println("Rolled values: "+dice);
+            System.out.println("Your attack value: " + attackValue);
+            if (attackValue > monsterDefense) {
+                System.out.println("Your attack was successful.");
+                monsterLife = monsterLife - yourDamage;
+                System.out.println("Monster's remaining Life Points: " + monsterLife);
+            } else {
+                System.out.println("Your attack was not successful.");
+            }
+        } else {
+            System.out.println("Monster attacks.");
+            int dice = generator.nextInt(6)+1 + generator.nextInt(6)+1;
+            int attackValue = monsterAttack + dice;
+            System.out.println("Rolled values:"+dice);
+            System.out.println("Monster's attack value: " + attackValue);
+            if (attackValue > yourDefense) {
+                System.out.println("Monster's attack was successful.");
+                yourLife = yourLife - monsterDamage;
+                System.out.println("Your remaining Life Points: " + yourLife);
+            } else {
+                System.out.println("Monster's attack was not successful.");
+            }
+        }
+    }
+}
