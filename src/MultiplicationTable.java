c00c0e504c8b29007d07e3c5d287b345a36f429d
Vishal Mudhiraj <vishalmudhiraj71@gmail.com>
2018-05-08 13:45:07 +0530

    MultiplicationTable

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vishal
+ */
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%3d", i*j); 
+            }
+            System.out.println();
+        }
+    }
+}
